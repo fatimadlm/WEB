949d9ab7e19d708a135bd4a20535da351ff09590
fatimadlm <99411119+fatimadlm@users.noreply.github.com>
2025-11-09 19:43:27 +0100

    Añadido post de TuUsuario

--- Parte 1/WEB/js/Home.js
+++ Parte 1/WEB/js/Home.js
@@ -4,12 +4,68 @@ const currentUserAvatar = "../Imagenes/Avatar_TuUsuario.jpg"; // Imagen del usua
 
 //  Array de posts 
 let posts = [
-  { username: "@Juan", avatar: "../Imagenes/Avatar4.jpeg", time: "Hace 2 horas", content: "Hoy preparé una lasaña casera con salsa bechamel 🤤. ¡Aquí mi receta!", img: "../Imagenes/Lasanna.png", likes: 12, liked: false, comments: [{ username: "@CocineraAna", text: "¡Se ve deliciosa!" }] },
-  { username: "@Ana", avatar: "../Imagenes/Avatar1.jpg", time: "Hace 5 horas", content: "Pan casero con masa madre 😍 recién salido del horno.", img: "../Imagenes/Pan.jpg", likes: 42, liked: false, comments: [] },
-  { username: "@Mario", avatar: "../Imagenes/Avatar3.jpg", time: "Hace 1 hora", content: "Tacos al pastor 🌮, ¡los mejores de la ciudad!", img: "../Imagenes/Tacos.jpg", likes: 35, liked: false, comments: [{ username: "@Laura", text: "¡Quiero probarlos!" }] },
-  { username: "@Laura", avatar: "../Imagenes/Avatar5.jpeg", time: "Hace 3 horas", content: "Brownies de chocolate 🍫 con nueces, recién horneados.", img: "../Imagenes/Brownie.jpg", likes: 28, liked: false, comments: [] },
-  { username: "@Caro", avatar: "../Imagenes/Avatar6.jpeg", time: "Hace 6 horas", content: "Ensalada fresca de quinoa y aguacate 🥗, ideal para el verano.", img: "../Imagenes/Ensalada.jpg", likes: 18, liked: false, comments: [] }
+  { 
+    username: "@Juan", 
+    avatar: "../Imagenes/Avatar4.jpeg", 
+    time: "Hace 2 horas", 
+    content: "Hoy preparé una lasaña casera con salsa bechamel 🤤. ¡Aquí mi receta!", 
+    img: "../Imagenes/Lasanna.png", 
+    likes: 12, 
+    liked: false, 
+    comments: [{ username: "@CocineraAna", text: "¡Se ve deliciosa!" }] 
+  },
+  { 
+    username: "@Ana", 
+    avatar: "../Imagenes/Avatar1.jpg", 
+    time: "Hace 5 horas", 
+    content: "Pan casero con masa madre 😍 recién salido del horno.", 
+    img: "../Imagenes/Pan.jpg", 
+    likes: 42, 
+    liked: false, 
+    comments: [] 
+  },
+  { 
+    username: "@Mario", 
+    avatar: "../Imagenes/Avatar3.jpg", 
+    time: "Hace 1 hora", 
+    content: "Tacos al pastor 🌮, ¡los mejores de la ciudad!", 
+    img: "../Imagenes/Tacos.jpg", 
+    likes: 35, 
+    liked: false, 
+    comments: [{ username: "@Laura", text: "¡Quiero probarlos!" }] 
+  },
+  { 
+    username: "@Laura", 
+    avatar: "../Imagenes/Avatar5.jpeg", 
+    time: "Hace 3 horas", 
+    content: "Brownies de chocolate 🍫 con nueces, recién horneados.", 
+    img: "../Imagenes/Brownie.jpg", 
+    likes: 28, 
+    liked: false, 
+    comments: [] 
+  },
+  { 
+    username: "@Caro", 
+    avatar: "../Imagenes/Avatar6.jpeg", 
+    time: "Hace 6 horas", 
+    content: "Ensalada fresca de quinoa y aguacate 🥗, ideal para el verano.", 
+    img: "../Imagenes/Ensalada.jpg", 
+    likes: 18, 
+    liked: false, 
+    comments: [] 
+  },
+  { 
+    username: "@TuUsuario", 
+    avatar: "../Imagenes/Avatar1.jpg", 
+    time: "Hace 1 día", 
+    content: "Mi nueva receta: pasta carbonara cremosa", 
+    img: "../Imagenes/carbonara.jpg", 
+    likes: 12, 
+    liked: false, 
+    comments: [] 
+  }
 ];
+
 
 //Renderizar posts 
 function renderPosts(postsArray) {
