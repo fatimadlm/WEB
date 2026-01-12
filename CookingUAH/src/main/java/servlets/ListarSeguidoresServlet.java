b99bffc268ae8fea6d7d1a042279843528ac955e
DomingoJarillo <99411146+DomingoJarillo@users.noreply.github.com>
2026-01-12 22:38:54 +0100

    Update ListarSeguidoresServlet.java

--- CookingUAH/src/main/java/servlets/ListarSeguidoresServlet.java
+++ CookingUAH/src/main/java/servlets/ListarSeguidoresServlet.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package servlets;
 
 import java.io.IOException;
@@ -15,10 +12,7 @@ import java.util.List;
 import modelo.User;
 import modelo.UserDAO;
 
-/**
- *
- * @author fatim
- */
+
 @WebServlet("/ListarSeguidoresServlet")
 public class ListarSeguidoresServlet extends HttpServlet {
    @Override
